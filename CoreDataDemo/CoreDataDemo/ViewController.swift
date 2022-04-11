0fd4dc69c01e8e5148c95574e7cea3c8687f92e4
Gabriel Zempoalteca <gzempoalteca@acculynx.com>
2022-04-10 20:36:03 -0500

    feat: Update project

---
+++ CoreDataDemo/CoreDataDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataDemo
+//
+//  Created by Gabriel Zempoalteca Garrido on 10/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
