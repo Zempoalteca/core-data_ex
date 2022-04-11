dfb7390388b67b9cefa74b391f32f152d86164d7
Gabriel Zempoalteca <gzempoalteca@acculynx.com>
2022-04-10 20:15:39 -0500

    feat: Initial project

---
+++ CoreData/CoreData/CoreDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataApp.swift
+//  CoreData
+//
+//  Created by Gabriel Zempoalteca Garrido on 10/04/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
